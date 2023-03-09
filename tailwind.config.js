a8e1519523da0f2847524aba4e1c459210cd5ccc
dicefoxs <aardivan20@gmail.com>
2023-03-09 18:00:11 +0700

    menambahkan file baru

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      screens: {
+        '2xl': '1320px',
+      },
+      colors: {
+        primary: '#14b8a6',
+        dark: '#0f172a',
+      },
+    },
+  },
+  plugins: [],
+}
