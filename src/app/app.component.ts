90c509daa7fd627daa8969bc37a2c8db4f91aa1f
dicefoxs <aardivan20@gmail.com>
2023-03-10 00:04:28 +0700

    Menambahkan navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  @ViewChild('header') header: any;
+  @HostListener('window:scroll', []) onWindowScroll(){
+    let headers = this.header.nativeElement;
+    const fixedNav = headers.offsetTop;
+    if(window.pageYOffset > fixedNav){
+      headers.classList.add('navbar-fixed');
+    }else{
+      headers.classList.remove('navbar-fixed');
+    }
+  }
   title = 'my-project';
+  @ViewChild('hamburger') hamburger: any;
+  @ViewChild('navMenu') navMenu: any;
+
+  ngAfterViewInit(){
+    // Hamburger
+    let burger = this.hamburger.nativeElement;
+    let menuNav = this.navMenu.nativeElement;
+    burger.addEventListener('click', function(){
+      burger.classList.toggle('hamburger-active');
+      menuNav.classList.toggle('hidden');
+    });
+  }
+    
 }
+
